38c18ec57e01f1fdc5b19a7a3a8b62ffb3dd8c4d
Raynald <raynald@easi-services.fr>
2017-11-12 12:45:24 +0100

    Remove dependency to obsolete SonataExtendedAdmin Bundle

--- Admin/MenuAdmin.php
+++ Admin/MenuAdmin.php
@@ -207,7 +207,7 @@ class MenuAdmin extends Admin
                 'actions' => array(
                     'edit'      => array(),
                     'delete'    => array(),
-                    'activate'  => array('template' => 'SkillbertoSonataExtendedAdminBundle:Admin:list__action_activate.html.twig'),
+                    'activate'  => array('template' => 'SkillbertoSonataPageMenuBundle:Admin:list__action_activate.html.twig'),
                     'move'      => array('template' => 'SkillbertoSonataPageMenuBundle:Admin:list__action_sort.html.twig')
                     )
                 )
--- Controller/MenuAdminController.php
+++ Controller/MenuAdminController.php
@@ -2,9 +2,11 @@
 
 namespace Skillberto\SonataPageMenuBundle\Controller;
 
-use Skillberto\SonataExtendedAdminBundle\Controller\CRUDController as Controller;
+use Sonata\AdminBundle\Controller\CRUDController as Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\File\Exception\AccessDeniedException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Exception\RouteNotFoundException;
 
 class MenuAdminController extends Controller
@@ -41,5 +43,37 @@ class MenuAdminController extends Controller
         }
 
         return new RedirectResponse($this->admin->generateUrl('list'));
+    }
+    
+    /**
+     * Activate or inactivate the object
+     *
+     * @throws Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+     * @return Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function activateAction()
+    {
+        $object = $this->getObject();
+        
+        if (method_exists($object, 'setActive') && method_exists($object, 'getActive')) {
+            $object->setActive(($object->getActive()==1) ? 0 : 1);
+        }
+        
+        $this->admin->update($object);
+        
+        return new RedirectResponse($this->admin->generateUrl('list'));
+    }
+    
+    protected function getObject($objectId = NULL)
+    {
+        $id = $this->get('request')->get($this->admin->getIdParameter());
+        
+        $object = $this->admin->getObject(empty($objectId) ? $id : $objectId);
+        
+        if (!$object) {
+            throw new NotFoundHttpException(sprintf('unable to find the object with id : %s', $id));
+        }
+        
+        return $object;
     }
 }
